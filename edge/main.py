848e776c80fb4e27de4a94ecbf0df67c691990f9
nickleigh <nickleigh05@icloud.com>
2026-06-10 22:44:36 -0700

    small updates for re-id people

--- edge/main.py
+++ edge/main.py
@@ -1,20 +1,26 @@
+import time
+
 import cv2
 from ultralytics import YOLO
+from tracker import Tracker
 
 
 model = YOLO('yolov8n.pt') # Load a pretrained YOLO model (you can choose n, s, m, l, or x versions)
 
 #VIDEO_PATH = 'data/footage.mp4'     # swap this file path for footage 
-VIDEO_PATH = 0                     # swap this for webcame use
+VIDEO_PATH = 0                     # swap this for webcam use
 
 def main():
 
     cap = cv2.VideoCapture(VIDEO_PATH)
 
     if not cap.isOpened():
-        print(f"Erorr: Video was not opened at {VIDEO_PATH}")
+        print(f"Error: Video was not opened at {VIDEO_PATH}")
         return
-    
+
+    tracker = Tracker()
+    last_print = 0.0
+
     while True:
         ret, frame = cap.read()
 
@@ -24,6 +30,12 @@ def main():
 
         
         results = model.track(frame, persist=True, classes=[0], verbose=False)
+        tracker.update(results, frame)   # frame needed so the tracker can crop people for re-ID
+        
+        if time.time() - last_print >= 1.0:
+            tracker.print_all()
+            last_print = time.time()
+        #print(results[0].boxes)
         annotated_frame = results[0].plot()
         cv2.imshow('Cloud Collar', annotated_frame)
 
